a765bcfa1e3fb12b6c094c653fa747970734b411
0SyedAli <sa8449006@gmail.com>
2026-05-16 05:00:23 +0500

    Update Navbar.jsx

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -57,8 +57,8 @@ export default function Navbar() {
                         <Link href="#!">
                             <Image
                                 src="/images/logo-white.png"
-                                width={202.67}
-                                height={76.1}
+                                width={250.67}
+                                height={100}
                                 alt="WIG Out"
                             />
                         </Link>
